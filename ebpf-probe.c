1180d2d6edc7e19330554399b20643c7e379587b
bruce-mig <brucetmig@icloud.com>
2025-11-25 10:47:35 +0200

    feat: add kprobes for sys_clone and sys_open events

--- ebpf-probe.c
+++ ebpf-probe.c
@@ -1,17 +1,26 @@
 #include <bcc/proto.h>
+#include <linux/fs.h>
 #include <linux/sched.h>
 #include <uapi/linux/ptrace.h>
 
-struct data_t {
+struct clone_data_t {
    u32 pid;                  // process id
    u32 ppid;                 // parent process id
    char comm[TASK_COMM_LEN]; // process name
 };
 
-BPF_PERF_OUTPUT(events); // define a perf event output buffer
+struct open_data_t {
+   u32 pid;
+   u64 timestamp;
+   char comm[TASK_COMM_LEN];
+   char filename[NAME_MAX];
+};
+
+BPF_PERF_OUTPUT(clone_events); // define a perf output for clone events
+BPF_PERF_OUTPUT(open_events);  // define a perf output for open events
 
 int kprobe__sys_clone(void *ctx) {
-   struct data_t data = {};
+   struct clone_data_t data = {};
    struct task_struct *task;
 
    task = (struct task_struct *)bpf_get_current_task();
@@ -19,6 +28,24 @@ int kprobe__sys_clone(void *ctx) {
    data.ppid = task->real_parent->tgid;
    bpf_get_current_comm(&data.comm, sizeof(data.comm)); // get process name
 
-   events.perf_submit(ctx, &data, sizeof(data)); // submit event to user space
+   clone_events.perf_submit(ctx, &data,
+                            sizeof(data)); // submit event to user space
+   return 0;
+}
+
+int kprobe__do_sys_openat2(struct pt_regs *ctx, int dfd,
+                           const char __user *filename, int flags,
+                           umode_t mode) {
+   struct open_data_t data = {};
+
+   data.pid = bpf_get_current_pid_tgid();
+   data.timestamp = bpf_ktime_get_ns();
+   bpf_get_current_comm(&data.comm, sizeof(data.comm));
+
+   bpf_probe_read_user_str(&data.filename, sizeof(data.filename),
+                           filename); // read filename from user space
+
+   open_events.perf_submit(ctx, &data,
+                           sizeof(data)); // submit event to user space
    return 0;
 }
--- ebpf-runner.py
+++ ebpf-runner.py
@@ -3,18 +3,23 @@
 from bcc import BPF
 from pathlib import Path
 
-def process_event(cpu, data, size):
-      event = bpf["events"].event(data)  # Define the event structure
+def process_clone_event(cpu, data, size):
+      event = bpf["clone_events"].event(data)  # Define the event structure
       print(f"Process {event.comm.decode()} (PID: {event.pid}, PPID: {event.ppid}) called sys_clone")
+
+def process_open_event(cpu, data, size):
+      event = bpf["open_events"].event(data)  # Define the event structure
+      print(f"[{event.timestamp / 1e9:.6f}] Process {event.comm.decode()} (PID: {event.pid}) called sys_open with filename: {event.filename.decode()}")
 
 bpf_source = Path('ebpf-probe.c').read_text()
 bpf = BPF(text=bpf_source)
 
-bpf["events"].open_perf_buffer(process_event)
+bpf["clone_events"].open_perf_buffer(process_clone_event)
+bpf["open_events"].open_perf_buffer(process_open_event)
 print("Monitoryng sys_clone calls... Press Ctrl+C to exit.")
 
 while True:
     try:
-        bpf.perf_buffer_poll()  # Poll for events
+        bpf.perf_buffer_poll()  # Poll for events  
     except KeyboardInterrupt:
         break
